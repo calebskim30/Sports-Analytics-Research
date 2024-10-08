5c7de8fe90a632043a1c1ce557a71c40af5f6290
Caleb <calebskim30@gmail.com>
2024-10-07 20:21:14 -0700

    fixed

--- maxpreps_data/maxpreps_stats.py
+++ maxpreps_data/maxpreps_stats.py
@@ -3,8 +3,8 @@ import requests
 pd.set_option('display.max_columns', None)
 import numpy as np
 
-#test_url = 'https://production.api.maxpreps.com/gatewayweb/react/team-season-player-stats/rollup/v1?teamId=cb3c4816-4749-4381-8c4c-5613ef8c89c9&sportSeasonId=77be7c75-cdf9-483d-867f-ea2af557e731'
-test_url = 'https://stats.nba.com/stats/leagueLeaders?ActiveFlag=No&LeagueID=00&PerMode=Totals&Scope=S&Season=All%20Time&SeasonType=Playoffs&StatCategory=PTS'
+test_url = 'https://production.api.maxpreps.com/gatewayweb/react/team-season-player-stats/rollup/v1?teamId=cb3c4816-4749-4381-8c4c-5613ef8c89c9&sportSeasonId=77be7c75-cdf9-483d-867f-ea2af557e731'
+#test_url = 'https://stats.nba.com/stats/leagueLeaders?ActiveFlag=No&LeagueID=00&PerMode=Totals&Scope=S&Season=All%20Time&SeasonType=Playoffs&StatCategory=PTS'
 url_json = requests.get(url=test_url).json()
 
 
